c80325cb56718eed0f66e29f1fb64efaae89b8ef
Divya-Varatharajan <53460282+Divya-Varatharajan@users.noreply.github.com>
2020-09-18 10:54:58 +0530

    Update CscExamples.java

---
+++ CscExamples.java
@@ -0,0 +1,23 @@
+public void setquestionNo(int questionNo){
+  this.questionNo=questionNo;
+}
+
+public void setCategory(String category){
+  this.category=category;
+}
+
+public void setText(String text){
+  this.text=text;
+}
+
+public int getQuestionNo(){
+  return questionNo;
+}
+
+public String getCategory(){
+  return category;
+}
+
+public String getText(){
+  return text;
+}
